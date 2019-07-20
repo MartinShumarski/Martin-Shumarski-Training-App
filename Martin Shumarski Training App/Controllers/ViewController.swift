21036b7cdeeb5b7d448dcee07da93d27f1e25950
Martin Shumarski <ms@martin-shumarskis-macbook-pro.local>
2019-07-20 18:47:06 +0300

    Initial Commit

---
+++ Martin Shumarski Training App/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Martin Shumarski Training App
+//
+//  Created by Martin Shumarski on 4.02.19.
+//  Copyright © 2019 Martin Shumarski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
