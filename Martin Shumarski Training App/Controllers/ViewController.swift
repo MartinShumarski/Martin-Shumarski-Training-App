9a4dc91dc74276ed9927daa35eddc4902c960257
Martin Shumarski <ms@martin-shumarskis-macbook-pro.local>
2019-07-25 20:51:36 +0300

    Added userID placeholder functionality + Set UserID + Set User Attributes

--- Martin Shumarski Training App/Controllers/ViewController.swift
+++ Martin Shumarski Training App/Controllers/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Leanplum
 
 class ViewController: UIViewController {
  
@@ -28,6 +29,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUserIdPlaceholder()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -36,5 +38,64 @@ class ViewController: UIViewController {
         scrollView.contentSize = CGSize(width: 320, height: 968)
     }
 
-}
+    // MARK: Sets placeholder for the userID text field
+    func setUserIdPlaceholder () {
+        var currentID : String = ""
+        currentID = Leanplum.userId()
+        userId.placeholder = "userID: \(currentID)"
+        
+    }
+    
+    // MARK: Change the UserID -> login
+    
+    @IBAction func changeUserID(_ sender: UIButton) {
+        
+        if (userId.text?.count != 0) && (userId.text != Leanplum.userId()) {
+            Leanplum.setUserId(userId.text)
+            setUserIdPlaceholder()
+            userId.text = ""
+            
+            DispatchQueue.main.async {
+                    self.userId.resignFirstResponder()
+            }
+        
+            
+        }
+        
+    }
+    
+    
+    //MARK : Set User Attributes
+    
+    @IBAction func setUserAttributes(_ sender: UIButton) {
+        
+        let newAttributeName = attributeName.text! as NSString
+        let newAttributeValue = attributeValue.text! as NSString
+        
+        if attributeName.text?.count != 0 && attributeValue.text?.count != 0 {
+            if let integerValue = NSInteger(attributeValue.text!) {
+                Leanplum.setUserAttributes([newAttributeName:integerValue])
+            } else {
+                Leanplum.setUserAttributes([newAttributeName:newAttributeValue])
+            }
+            
+            attributeValue.text = ""
+            attributeName.text = ""
+            
+        }
+        
+        
+//        DispatchQueue.main.async {
+//            self.attributeValue.resignFirstResponder()
+        }
+        
+        
+    }
+    
+    
+    
+
+
+
+
 
